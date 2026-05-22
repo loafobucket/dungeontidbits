17f60fa3f4f46d15efd26ac579db8769ca44ea65
loafobucket <50703lfps@gmail.com>
2026-05-22 10:35:14 +0800

    Attempted to follow tutorials

---
+++ src/main/java/com/loafobucket/dungeontidbits/DungeonTidbits.java
@@ -0,0 +1,45 @@
+package com.loafobucket.dungeontidbits;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(DungeonTidbits.MOD_ID)
+public class DungeonTidbits {
+    public static final String MOD_ID = "dungeontidbits";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public DungeonTidbits(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+}
