2fca82a694ded7243832482111b1b5f9ccb9f864
loafobucket <50703lfps@gmail.com>
2026-05-29 18:01:36 +0800

    Room key functionality?

--- src/main/java/com/loafobucket/dungeontidbits/item/custom/RoomKeyItem.java
+++ src/main/java/com/loafobucket/dungeontidbits/item/custom/RoomKeyItem.java
@@ -2,13 +2,18 @@ package com.loafobucket.dungeontidbits.item.custom;
 
 import com.loafobucket.dungeontidbits.block.ModBlocks;
 import com.loafobucket.dungeontidbits.component.ModDataComponents;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
@@ -32,23 +37,23 @@ public class RoomKeyItem extends Item {
     @Override
     public InteractionResult useOn(UseOnContext context) {
         Level level = context.getLevel();
-
         Block clickedBlock = level.getBlockState(context.getClickedPos()).getBlock();
         Integer halldepth = context.getItemInHand().get(ModDataComponents.HALLDEPTH);
         BlockPos blockpos = context.getClickedPos().relative(context.getClickedFace().getOpposite(), halldepth);
         ResourceLocation roomdata = ResourceLocation.parse(context.getItemInHand().get(ModDataComponents.ROOMDATA));
         Integer roomdepth = context.getItemInHand().get(ModDataComponents.ROOMDEPTH);
-        //i quite literally have no idea what happened here but it stopped showing as an error so i guess thats good
-        ResourceKey<StructureTemplatePool> pool = ResourceKey.create(Registries.TEMPLATE_POOL, roomdata);
-        Registry<StructureTemplatePool> registry = level.registryAccess().registryOrThrow(Registries.TEMPLATE_POOL);
-        Holder<StructureTemplatePool> holder = registry.getHolderOrThrow(pool);
-        //the amount of yellow here is concerning
+        ResourceLocation target = ResourceLocation.parse("minecraft:empty");
         boolean keepJigsaws = false;
 
         if(VOID_MAP.containsKey(clickedBlock)) {
             if (!level.isClientSide()) {
+                ResourceKey<StructureTemplatePool> pool = ResourceKey.create(Registries.TEMPLATE_POOL, roomdata);
+                Registry<StructureTemplatePool> registry = level.registryAccess().registryOrThrow(Registries.TEMPLATE_POOL);
+                Holder<StructureTemplatePool> holder = registry.getHolderOrThrow(pool);
                 ServerLevel serverlevel = (ServerLevel) level;
-                JigsawPlacement.generateJigsaw(serverlevel, holder, roomdata, roomdepth, blockpos, keepJigsaws);
+                JigsawPlacement.generateJigsaw(serverlevel, holder, target, roomdepth, blockpos, keepJigsaws);
+
+                level.playSound(null, context.getClickedPos(), SoundEvents.AMETHYST_BLOCK_BREAK, SoundSource.BLOCKS);
             }
         }
         return super.useOn(context);
